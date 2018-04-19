78118de87bffd8df87a2a5959ff0610f5c6c54af
ilammy <a.lozovsky@gmail.com>
2018-04-20 00:16:30 +0300

    Hook installation interface
    This demonstrates how to install hooks (for clone() and execve()
    system call handlers). The hooks themselves are not yet installed.

--- ftrace_hook.c
+++ ftrace_hook.c
@@ -7,14 +7,178 @@
 #define pr_fmt(fmt) "ftrace_hook: " fmt
 
 #include <linux/kernel.h>
+#include <linux/linkage.h>
 #include <linux/module.h>
+#include <linux/slab.h>
+#include <linux/uaccess.h>
 
 MODULE_DESCRIPTION("Example module hooking clone() and execve() via ftrace");
 MODULE_AUTHOR("ilammy <a.lozovsky@gmail.com>");
 MODULE_LICENSE("GPL");
 
+/*
+ * struct fh_hook - describes a single hook to install
+ * @name: name of the function to hook
+ * @func: pointer to the function to execute instead
+ * @orig: pointer to the location where to save a pointer
+ *        to the original function
+ *
+ * The user should fill in only &name, &hook, &orig fields.
+ * Other fields are considered implementation details.
+ */
+struct fh_hook {
+	const char *name;
+	void *func;
+	void *orig;
+};
+
+/**
+ * fh_install_hooks() - register and enable a single hook
+ * @hook: a hook to install
+ *
+ * Returns: zero on success, negative error code otherwise.
+ */
+int fh_install_hook(struct fh_hook *hook)
+{
+	return 0;
+}
+
+/**
+ * fh_remove_hooks() - disable and unregister a single hook
+ * @hook: a hook to remove
+ */
+void fh_remove_hook(struct fh_hook *hook)
+{
+}
+
+/**
+ * fh_install_hooks() - register and enable multiple hooks
+ * @hooks: array of hooks to install
+ * @count: number of hooks to install
+ *
+ * If some hooks fail to install then all hooks will be removed.
+ *
+ * Returns: zero on success, negative error code otherwise.
+ */
+int fh_install_hooks(struct fh_hook *hooks, size_t count)
+{
+	int err;
+	size_t i;
+
+	for (i = 0; i < count; i++) {
+		err = fh_install_hook(&hooks[i]);
+		if (err)
+			goto error;
+	}
+
+	return 0;
+
+error:
+	while (i != 0) {
+		fh_remove_hook(&hooks[--i]);
+	}
+
+	return err;
+}
+
+/**
+ * fh_remove_hooks() - disable and unregister multiple hooks
+ * @hooks: array of hooks to remove
+ * @count: number of hooks to remove
+ */
+void fh_remove_hooks(struct fh_hook *hooks, size_t count)
+{
+	size_t i;
+
+	for (i = 0; i < count; i++)
+		fh_remove_hook(&hooks[i]);
+}
+
+#ifndef CONFIG_X86_64
+#error Currently only x86_64 architecture is supported
+#endif
+
+static asmlinkage long (*real_sys_clone)(unsigned long clone_flags,
+		unsigned long newsp, int __user *parent_tidptr,
+		int __user *child_tidptr, unsigned long tls);
+
+static asmlinkage long fh_sys_clone(unsigned long clone_flags,
+		unsigned long newsp, int __user *parent_tidptr,
+		int __user *child_tidptr, unsigned long tls)
+{
+	long ret;
+
+	pr_info("clone() before\n");
+
+	ret = real_sys_clone(clone_flags, newsp, parent_tidptr,
+		child_tidptr, tls);
+
+	pr_info("clone() after: %ld\n", ret);
+
+	return ret;
+}
+
+static char *duplicate_filename(const char __user *filename)
+{
+	char *kernel_filename;
+
+	kernel_filename = kmalloc(4096, GFP_KERNEL);
+	if (!kernel_filename)
+		return NULL;
+
+	if (strncpy_from_user(kernel_filename, filename, 4096) < 0) {
+		kfree(kernel_filename);
+		return NULL;
+	}
+
+	return kernel_filename;
+}
+
+static asmlinkage long (*real_sys_execve)(const char __user *filename,
+		const char __user *const __user *argv,
+		const char __user *const __user *envp);
+
+static asmlinkage long fh_sys_execve(const char __user *filename,
+		const char __user *const __user *argv,
+		const char __user *const __user *envp)
+{
+	long ret;
+	char *kernel_filename;
+
+	kernel_filename = duplicate_filename(filename);
+
+	pr_info("execve() before: %s\n", kernel_filename);
+
+	kfree(kernel_filename);
+
+	ret = real_sys_execve(filename, argv, envp);
+
+	pr_info("execve() after: %ld\n", ret);
+
+	return ret;
+}
+
+static struct fh_hook demo_hooks[] = {
+	{
+		.name = "sys_clone",
+		.func = fh_sys_clone,
+		.orig = &real_sys_clone,
+	},
+	{
+		.name = "sys_execve",
+		.func = fh_sys_execve,
+		.orig = &real_sys_execve,
+	},
+};
+
 static int fh_init(void)
 {
+	int err;
+
+	err = fh_install_hooks(demo_hooks, ARRAY_SIZE(demo_hooks));
+	if (err)
+		return err;
+
 	pr_info("module loaded\n");
 
 	return 0;
@@ -23,6 +187,8 @@ module_init(fh_init);
 
 static void fh_exit(void)
 {
+	fh_remove_hooks(demo_hooks, ARRAY_SIZE(demo_hooks));
+
 	pr_info("module unloaded\n");
 }
 module_exit(fh_exit);
